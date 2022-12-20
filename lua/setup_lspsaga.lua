3e88c003a55896188e0e8c73bb4eba306f930d07
Alex <silvercircle@gmail.com>
2022-12-20 16:18:22 +0100

    new features setup

---
+++ lua/setup_lspsaga.lua
@@ -0,0 +1,8 @@
+require('lspsaga').init_lsp_saga({
+  code_action_lightbulb = { enable = false },
+  show_outline = {
+    win_width=36,
+    auto_refresh = true,
+    auto_preview = false
+  }
+})
---
+++ lua/setup_noice.lua
@@ -0,0 +1,101 @@
+ require("noice").setup({
+  lsp = {
+    progress = { enabled = false },
+    hover = { enabled = false },
+    signature = { enabled = false },
+  },
+  cmdline = {
+    enabled = true,
+  },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = true, -- enables the Noice messages UI
+    view = "cmdline", -- default view for messages
+    view_error = "notify", -- view for errors
+    view_warn = "notify", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+  },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        kind = "search_count",
+      },
+      opts = { skip = true },
+    },
+  },
+  views = {
+    notify = {
+      opts = { merge = true, animate = false },
+    },
+    popup = {
+      size = { height = "auto", width = "80%" },
+    },
+    messages = {
+      position = "top",
+      relative = "editor",
+      size = { width = "auto", height = "auto" },
+    },
+    cmdline_popup = {
+      position = {
+        row = 20,
+        col = "50%",
+      },
+      size = {
+        width = "auto",
+        height = "auto",
+      },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = 23,
+        col = "50%",
+      },
+      size = {
+        width = 80,
+        height = "auto",
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "FloatBorder" },
+      },
+    },
+    mini = {
+      timeout = 4000,
+    },
+    cmdline = {
+      timeout = 5000,
+      close = {
+        keys = { "q" },
+      },
+      backend = "popup",
+      relative = "editor",
+      position = {
+        row = "100%",
+        col = "50%",
+      },
+      size = {
+        height = "auto",
+        width = "100%",
+      },
+      border = {
+        style = "none",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = {
+          Normal = "NormalFloat",
+          FloatBorder = "NormalFloat",
+          IncSearch = "",
+          Search = "",
+        },
+      },
+    },
+  },
+})
