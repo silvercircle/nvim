9256c2f0d34c8393f96424657d921a742d4ff017
Alex <silvercircle@gmail.com>
2022-12-30 23:12:59 +0100

    fixes

--- lua/setup_lsp.lua
+++ lua/setup_lsp.lua
@@ -297,6 +297,13 @@ do
     border = "single",
   })
 
+  vim.lsp.handlers['workspace/diagnostic/refresh'] = function(_, _, ctx)
+    local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.diagnostic.reset(ns, bufnr)
+    return true
+  end
+
   vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
     local bufnr, winnr = lsp_handlers_hover(err, result, ctx, config)
     return bufnr, winnr
