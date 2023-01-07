768e92fe1d3e66df45bb27255087343b719b33fa
Alex <silvercircle@gmail.com>
2023-01-07 01:08:13 +0100

    fix floatterm+lazygit. detect files not under git control

--- lua/setup_command_center.lua
+++ lua/setup_command_center.lua
@@ -218,7 +218,13 @@ Command_center.add({
     -- use the path of the current buffer to find the .git root. The LSP utils are useful for
     -- such a case.
     desc = "FloatTerm lazygit",
-    cmd = function() local cmd = "FloatermNew --name=GIT --width=0.9 --height=0.9 lazygit --path=" .. lsputil.root_pattern(".git")(vim.fn.expand("%:p")) vim.cmd(cmd) end,
+    cmd = function() local path = lsputil.root_pattern(".git")(vim.fn.expand("%:p"))
+      if path == nil then
+        vim.notify("This file is not under GIT version control", 3)
+      else
+        local cmd = "FloatermNew --name=GIT --width=0.9 --height=0.9 lazygit --path=" .. lsputil.root_pattern(".git")(vim.fn.expand("%:p")) vim.cmd(cmd)
+      end
+    end,
     keys = { "n", "<f6>", noremap },
     category = "@GIT"
   },
