25cefd9ec95a17a7407d817f4554afb8511512f3
Alex <silvercircle@gmail.com>
2023-02-22 18:06:32 +0100

    fixes

--- lua/local_utils/usplit.lua
+++ lua/local_utils/usplit.lua
@@ -1,17 +1,19 @@
 local globals = require("globals")
 
 local M = {}
-M.winid = nil
-M.bufid = nil
-M.autocmds_valid = nil
+M.winid = nil             -- window id
+M.bufid = nil             -- buffer id
+M.autocmds_valid = nil    -- auto command was set
 
+-- set auto command for the sysmon split. Watches WinClosed to react by removing the buffer when the
+-- window has been closed.
 function M.setup_auto()
   if M.autocmds_valid == true then
     return
   end
   M.autocmds_valid = true
   vim.api.nvim_create_augroup("SYSMONSplit", { clear = true })
-  vim.api.nvim_create_autocmd({ "WinClosed", "WinResized" }, {
+  vim.api.nvim_create_autocmd({ "WinClosed" }, {
     group = "SYSMONSplit",
     callback = function()
       if M.winid ~= nil and vim.api.nvim_win_is_valid(M.winid) == false then  -- window has disappeared
@@ -25,9 +27,9 @@ function M.setup_auto()
   })
 end
 
--- this opens a split next to the terminal split and launches an instance of glances
--- system monitor in it. See: https://nicolargo.github.io/glances/
-
+--- this opens a split next to the terminal split and launches an instance of glances
+--- system monitor in it. See: https://nicolargo.github.io/glances/
+--- @param _width number: the width of the split. Default is vim.g.config.sysmon.width
 function M.open(_width)
   local width = _width or vim.g.config.sysmon.width
   local wid = globals.findwinbyBufType("terminal")
