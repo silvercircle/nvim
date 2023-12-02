3bd2f30f63f15b9f368fb5691635b34e88166bcd
Alex <silvercircle@gmail.com>
2023-12-02 10:25:03 +0100

    dressing plugin: respect global border setting

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -43,7 +43,7 @@ local dressing_select_default = {
     show_numbers = false,
     -- These are passed to nvim_open_win
     -- anchor = "NW",
-    border = "rounded",
+    border = __Globals.perm_config.telescope_borders,
     -- 'editor' and 'win' will default to being centered
     relative = "editor",
 
@@ -100,7 +100,7 @@ require('dressing').setup({
 
     -- These are passed to nvim_open_win
     -- anchor = "SW",
-    border = "rounded",
+    border = __Globals.perm_config.telescope_borders,
     -- 'editor' and 'win' will default to being centered
     relative = "editor",
 
