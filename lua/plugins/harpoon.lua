b998952e3f4f7bafdade3c88b6a0ae7ae2afba1e
Alex <silvercircle@gmail.com>
2025-01-07 17:14:05 +0100

    updated config for harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,7 +5,14 @@ harpoon:setup({
     decode = false
   }
 })
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
@@ -29,4 +36,4 @@ local function toggle_telescope(harpoon_files)
     sorter = conf.generic_sorter({}),
   }):find()
 end
-vim.keymap.set({"n", "i"}, "<A-e>", function() toggle_telescope(harpoon:list()) end, {desc = "Open harpoon window" })
+vim.keymap.set({"n", "i"}, "<A-h>", function() toggle_telescope(harpoon:list()) end, {desc = "Open harpoon window" })
