5304aa56b077e39d7c3f53a26ec9f1aad0c0cd24
Alex <silvercircle@gmail.com>
2025-01-17 21:50:30 +0100

    make roslyn LSP semantic tokens work with both nvim-0.10 and 0.11

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,4 +1,4 @@
- local function monkey_patch_semantic_tokens(client)
+local function monkey_patch_semantic_tokens_11(client)
     -- NOTE: Super hacky... Don't know if I like that we set a random variable on
     -- the client Seems to work though ~seblj
     if client.is_hacked then
@@ -40,6 +40,64 @@
     end
 end
 
+local function monkey_patch_semantic_tokens_10(client)
+  if not client.is_hacked_roslyn then
+    client.is_hacked_roslyn = true
+
+    -- let the runtime know the server can do semanticTokens/full now
+    if client.server_capabilities.semanticTokensProvider then
+      client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
+        semanticTokensProvider = {
+          full = true,
+        },
+      })
+    end
+
+    -- -- monkey patch the request proxy
+    local request_inner = client.request
+    client.request = function(method, params, handler, req_bufnr)
+      if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
+        return request_inner(method, params, handler, req_bufnr)
+      end
+
+      local function find_buf_by_uri(search_uri)
+        local bufs = vim.api.nvim_list_bufs()
+        for _, buf in ipairs(bufs) do
+          local name = vim.api.nvim_buf_get_name(buf)
+          local uri = "file://" .. name
+          if uri == search_uri then
+            return buf
+          end
+        end
+      end
+
+      local doc_uri = params.textDocument.uri
+
+      local target_bufnr = find_buf_by_uri(doc_uri)
+      local line_count = vim.api.nvim_buf_line_count(target_bufnr)
+      local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count,
+        true)[1]
+
+      return request_inner("textDocument/semanticTokens/range", {
+          textDocument = params.textDocument,
+          range = {
+            ["start"] = {
+              line = 0,
+              character = 0,
+            },
+            ["end"] = {
+              line = line_count - 1,
+              character = string.len(last_line) - 1,
+            },
+          },
+        },
+        handler,
+        req_bufnr
+      )
+    end
+  end
+end
+
 local util = require 'lspconfig.util'
 require("roslyn").setup({
   config = {
@@ -51,7 +109,11 @@ require("roslyn").setup({
     on_attach = function(client, bufnr)
       require("nvim-navic").attach(client, bufnr)
       require("nvim-navbuddy").attach(client, bufnr)
-      monkey_patch_semantic_tokens(client)
+      if vim.fn.has("nvim-0.11") == 1 then
+        monkey_patch_semantic_tokens_11(client)
+      else
+        monkey_patch_semantic_tokens_10(client)
+      end
     end,
   },
   roslyn_version = "4.13.0-3.25054.1",
