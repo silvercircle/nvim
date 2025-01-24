b1f59591e4f85bb7dfba81c81f6e6eadd22b4947
Alex <silvercircle@gmail.com>
2025-01-24 20:21:24 +0100

    clangd lsp configuration updated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,6 +56,53 @@ if not configs.ada then
   }
 end
 
+--- clangd support funcions
+local function clangd_switch_source_header(bufnr)
+  local method_name = 'textDocument/switchSourceHeader'
+  bufnr = util.validate_bufnr(bufnr)
+  local client = util.get_active_client_by_name(bufnr, 'clangd')
+  if not client then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  client.request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
+local function clangd_symbol_info()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clangd_client = util.get_active_client_by_name(bufnr, 'clangd')
+  if not clangd_client or not clangd_client.supports_method 'textDocument/symbolInfo' then
+    return vim.notify('Clangd client not found', vim.log.levels.ERROR)
+  end
+  local win = vim.api.nvim_get_current_win()
+  local params = vim.lsp.util.make_position_params(win, clangd_client.offset_encoding)
+  clangd_client.request('textDocument/symbolInfo', params, function(err, res)
+    if err or #res == 0 then
+      -- Clangd always returns an error, there is not reason to parse it
+      return
+    end
+    local container = string.format('container: %s', res[1].containerName) ---@type string
+    local name = string.format('name: %s', res[1].name) ---@type string
+    vim.lsp.util.open_floating_preview({ name, container }, '', {
+      height = 2,
+      width = math.max(string.len(name), string.len(container)),
+      focusable = false,
+      focus = false,
+      border = 'single',
+      title = 'Symbol Info',
+    })
+  end, bufnr)
+end
+
 -- this was tsserver (will be deprecated in the future)
 lspconfig.ts_ls.setup({
   init_options = { hostInfo = 'neovim' },
@@ -121,14 +168,35 @@ local clangd_root_files = {
   'configure.ac', -- AutoTools
 }
 lspconfig.clangd.setup({
-  cmd = { 'clangd', "--header-insertion-decorators", "--malloc-trim", "--background-index", "--pch-storage=memory" },
+  cmd = { 'clangd', "--header-insertion-decorators", "--background-index", "--malloc-trim", "--pch-storage=memory", "--log=error" },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
   root_dir = function(fname)
     return util.root_pattern(unpack(clangd_root_files))(fname) or util.find_git_ancestor(fname)
   end,
   single_file_support = true,
   on_attach = On_attach,
-  capabilities = capabilities
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+  commands = {
+    ClangdSwitchSourceHeader = {
+      function()
+        clangd_switch_source_header(0)
+      end,
+      description = 'Switch between source/header',
+    },
+    ClangdShowSymbolInfo = {
+      function()
+        clangd_symbol_info()
+      end,
+      description = 'Show symbol info',
+    },
+  }
 })
 
 lspconfig.ada.setup({})
@@ -558,14 +626,12 @@ if vim.diagnostic then
   })
 end
 do
-  if vim.fn.has("nvim-0.10") == 0 then
-    vim.fn.sign_define("DiagnosticSignError", { text = "✘", texthl = "RedSign" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "YellowSign" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = "◉", texthl = "BlueSign" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "GreenSign" })
-  end
+  vim.fn.sign_define("DiagnosticSignError", { text = "✘", texthl = "RedSign" })
+  vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "YellowSign" })
+  vim.fn.sign_define("DiagnosticSignInfo", { text = "◉", texthl = "BlueSign" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "GreenSign" })
 end
--- set a border for the 
+-- set a border for the default hover and diagnostics windows
 require('lspconfig.ui.windows').default_options.border = __Globals.perm_config.float_borders
 
 if vim.g.tweaks.notifier == "nvim-notify" then
