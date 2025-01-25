51372c298d1a038632eeb78db9d2f908e98a431a
Alex <silvercircle@gmail.com>
2025-01-25 21:29:14 +0100

    add support for tinymist (typst language server)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -128,6 +128,17 @@ lspconfig.texlab.setup({
   capabilities = capabilities
 })
 
+lspconfig.tinymist.setup({
+  cmd = { "tinymist" },
+  filetypes = { 'typst' },
+  root_dir = function(fname)
+    return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+  end,
+  single_file_support = true,
+  on_attach = On_attach,
+  capabilities = capabilities
+})
+
 lspconfig.nim_langserver.setup({
   on_attach = On_attach,
   capabilities = capabilities,
