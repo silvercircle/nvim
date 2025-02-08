593003e05880b69bc7b7b60af5bd69ce559f221d
Alex <silvercircle@gmail.com>
2025-02-08 13:38:24 +0100

    retire more obsolete code

--- lua/inactive/cmdpalette.lua
+++ lua/inactive/cmdpalette.lua
@@ -2,21 +2,29 @@
 local M = {}
 local conf = {
   columns = {
-    desc = {width = 43, hl = "String"},
-    cat  = {width = 18, hl = "Class", align = "right"},
-    cmd  = {width = 14, hl = "DefaultLib", "right"},
-    keys = {width = 43, hl = "Operator"},
-    err  = {width = 0, hl = "DeepRedBold"}
-  }
+    desc = { width = 43, hl = "String" },
+    cat  = { width = 18, hl = "Class", align = "right" },
+    cmd  = { width = 14, hl = "DefaultLib", "right" },
+    keys = { width = 43, hl = "Operator" },
+    err  = { width = 0, hl = "DeepRedBold" }
+  },
+  picker = {
+    height = 20,
+    width = 120,
+    row = 10,
+    layout_preset = "vscode"
+  },
+  custom_layout = {}
 }
 
 M.pickeritems = {}
+M.layout = {}
 
 --- truncate a string to a maximum length, appending ellipses when necessary
 --- @param text string:       the string to truncate
 --- @param max_length integer:  the maximum length. must be at least 2
 ---                             (one character + ellipis)
---- @return string:           the truncated text
+--- @return string
 local function truncate(text, max_length)
   if max_length > 1 and vim.fn.strwidth(text) > max_length then
     return vim.fn.strcharpart(text, 0, max_length - 1) .. "…"
@@ -34,10 +42,10 @@ function M.add(entries)
     local pos = #M.pickeritems
     if l.cmd == nil or l.desc == nil then return end
     M.pickeritems[pos + 1] = {
-      cat = l.category or '',
+      cat = l.category or "",
       cmd = l.cmd,
       desc = l.desc,
-      text = (l.category or '') .. " " .. l.desc .. (type(l.cmd) == "string" and l.cmd or ""),
+      text = (l.category or "") .. " " .. l.desc .. (type(l.cmd) == "string" and l.cmd or ""),
       keys = l.keys or nil
     }
     if l.keys ~= nil then
@@ -53,13 +61,33 @@ function M.add(entries)
   end)
 end
 
+--- open the command palette, using a snacks picker.
 function M.open()
+  local s, Snacks = pcall(require, "snacks")
+  if s == false or Snacks == nil then
+    vim.notify("This plugin requires the snacks plugin")
+    return
+  end
+
   if #M.pickeritems == 0 then
     vim.notify("There are no commands to show.")
     return
   end
 
-  local Snacks = require("snacks")
+  if M.layout.preset == nil then
+    M.layout = {
+      preset = conf.picker.layout_preset,
+      preview = false,
+      layout = {
+        min_width = 10,
+        width = conf.picker.width,
+        min_height = 10,
+        height = conf.picker.height,
+        row = conf.picker.row
+      }
+    }
+  end
+
   local Align = Snacks.picker.util.align
 
   local oldmode = vim.fn.mode()
@@ -68,8 +96,8 @@ function M.open()
       return M.pickeritems
     end,
     focus = "input",
-    -- TODO: move to setup
-    layout = __Globals.gen_snacks_picker_layout( { width=120, height=25, row=5, input="bottom", prompt="Command Palette", preview=false } ),
+    -- TODO: move to setup and remove dependency from my config's global fns
+    layout = conf.custom_layout.layout ~= nil and conf.custom_layout or M.layout,
     sort = {
       fields = {
         'cat',
@@ -83,12 +111,10 @@ function M.open()
       local e = {}
       local c = conf.columns
       local keys = ""
-      local key_align
       local key_hl
 
       if item.keys == nil then
         keys = "<No keys defined>"
-        key_align = "center"
         key_hl = c.err.hl
       else
         if type(item.keys[1]) == "string" then
@@ -100,11 +126,10 @@ function M.open()
         else
           keys = "Unknown"
         end
-        key_align = "left"
         key_hl = c.keys.hl
       end
       e[#e + 1] = { Align(truncate(item.desc, c.desc.width), c.desc.width, {align = c.desc.align or "left"}), c.desc.hl }
-      e[#e + 1] = { Align(truncate(keys, c.keys.width), c.keys.width, {align = key_align}), key_hl }
+      e[#e + 1] = { Align(truncate(keys, c.keys.width), c.keys.width, {align = c.keys.align or "left"}), key_hl }
       e[#e + 1] = { Align(type(item.cmd) == "function" and "LUA Function" or item.cmd, c.cmd.width, {align = c.cmd.align or "left"}), c.cmd.hl }
       e[#e + 1] = { Align(item.cat, c.cat.width, {align=c.cat.align or "left"}), c.cat.hl }
       return e
@@ -123,9 +148,9 @@ end
 
 function M.setup(opts)
   opts = opts or {}
-  if opts.keys ~= nil then
-    M.add(opts.keys)
-    opts.keys = nil
+  if opts.commands ~= nil then
+    M.add(opts.commands)
+    opts.commands = nil
   end
   conf = vim.tbl_deep_extend("force", conf, opts)
 end
