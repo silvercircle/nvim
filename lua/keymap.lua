cb55c058b1bf1af94a72830653f7c029d7b32c13
Alex <silvercircle@gmail.com>
2025-02-17 09:32:35 +0100

    removed old keybind(s)

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -473,14 +473,6 @@ vim.g.setkey({ 'n', 'i', 't', 'v' }, utility_key .. '+', function()
   __Globals.toggle_outline_type()        -- toggle the outline plugin (aerial <> symbols-outline)
 end, "Toggle Outline plugin type")
 
-vim.g.setkey({ 'n', 'i', 't', 'v' }, '<A-n>', function()
-  if vim.g.tweaks.breadcrumb == "navic" then
-    require("nvim-navbuddy").open()
-  else
-    require("aerial").nav_open()
-  end
-end, "Open Navbuddy window")
-
 require("local_utils.marks").set_keymaps()
 --vim.cmd("nunmap <cr>")
 
