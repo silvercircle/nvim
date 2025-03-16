eddc0f23f39fd4e535be20b109c53626ddec2a9a
Alex <silvercircle@gmail.com>
2025-03-16 11:27:10 +0100

    update jdtls config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -41,7 +41,7 @@ elseif project_root ~= nil and #project_root > 1 then
   project_name = md5.tohex(hash:finish())
 end
 
-local workspace_dir = vim.fn.expand(Tweaks.jdtls.workspace_base) .. project_name
+local workspace_dir = vim.fn.expand(LSPDEF.jdtls.workspace_base) .. project_name
 
 if debug then vim.notify("Project name is: " .. project_name, 1) end
 
@@ -56,7 +56,7 @@ local config = {
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
-    Tweaks.jdtls.java_executable, -- or '/:path/to/java17_or_newer/bin/java'
+    LSPDEF.jdtls.java_executable, -- or '/:path/to/java17_or_newer/bin/java'
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
@@ -74,13 +74,13 @@ local config = {
     "-XX:MaxHeapFreeRatio=85",
     "-XX:ConcGCThreads=2",
     "-XX:ParallelGCThreads=2",
-    use_lombok and "-javaagent:" .. vim.fn.expand(Tweaks.jdtls.jdtls_install_dir) .. "lombok.jar" or "",
-    use_lombok and "-Xbootclasspath/a:" .. vim.fn.expand(Tweaks.jdtls.jdtls_install_dir) .. "lombok.jar" or "",
+    use_lombok and "-javaagent:" .. vim.fn.expand(LSPDEF.jdtls.jdtls_install_dir) .. "lombok.jar" or "",
+    use_lombok and "-Xbootclasspath/a:" .. vim.fn.expand(LSPDEF.jdtls.jdtls_install_dir) .. "lombok.jar" or "",
     "--add-modules=ALL-SYSTEM",
     "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-    "-jar", vim.fn.expand(Tweaks.jdtls.jdtls_install_dir) .. "plugins/org.eclipse.equinox.launcher_" .. Tweaks.jdtls.equinox_version .. ".jar",
-    "-configuration", vim.fn.expand(Tweaks.jdtls.jdtls_install_dir) .. Tweaks.jdtls.config,
+    "-jar", vim.fn.expand(LSPDEF.jdtls.jdtls_install_dir) .. "plugins/org.eclipse.equinox.launcher_" .. LSPDEF.jdtls.equinox_version .. ".jar",
+    "-configuration", vim.fn.expand(LSPDEF.jdtls.jdtls_install_dir) .. LSPDEF.jdtls.config,
     "-data", workspace_dir
   },
   capabilities = CGLOBALS.get_lsp_capabilities(),
