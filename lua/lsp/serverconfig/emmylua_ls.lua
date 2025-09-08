85853f8c62bf103c8c616413bf1c6791c6143b45
Alex <silvercircle@gmail.com>
2025-09-08 21:41:52 +0200

    merge #2

---
+++ lua/lsp/serverconfig/emmylua_ls.lua
@@ -0,0 +1,40 @@
+---@brief
+---
+--- https://github.com/EmmyLuaLs/emmylua-analyzer-rust
+---
+--- Emmylua Analyzer Rust. Language Server for Lua.
+---
+--- `emmylua_ls` can be installed using `cargo` by following the instructions[here]
+--- (https://github.com/EmmyLuaLs/emmylua-analyzer-rust?tab=readme-ov-file#install).
+---
+--- The default `cmd` assumes that the `emmylua_ls` binary can be found in `$PATH`.
+--- It might require you to provide cargo binaries installation path in it.
+
+return {
+  cmd = { 'emmylua_ls' },
+  filetypes = { 'lua' },
+  root_markers = {
+    '.luarc.json',
+    '.emmyrc.json',
+    '.luacheckrc',
+    '.git',
+  },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+        requirePattern = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        }
+      },
+      workspace = {
+        library = {"$VIMRUNTIME"}
+      },
+      diagnostics = {
+        disable = {"unnecessary-if", "param-type-not-match", "need-check-nil"}
+      }
+    }
+  },
+  workspace_required = false,
+}
