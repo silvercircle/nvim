89ca5920a75c347b8f05d43f4c76f23771ff11e7
Alex <silvercircle@gmail.com>
2025-12-29 12:42:40 +0100

    ada fix

--- lua/lsp/serverconfig/ada_ls.lua
+++ lua/lsp/serverconfig/ada_ls.lua
@@ -3,10 +3,12 @@ local util = require("lsp.utils")
 ---@type vim.lsp.Config
 return {
   cmd = { LSPDEF.serverconfigs["ada_ls"].cmd[1] },
-  root_markers = { ".als.json"},
   filetypes = { "ada" },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern(".als.json", ".git", "alire.toml", "*.gpr")(fname))
+    -- vim.notify("Fname is: " .. fname)
+    local root = util.root_pattern(".als.json", ".git", "alire.toml", "*.gpr")(fname)
+    -- vim.notify("Root is: " .. root)
+    on_dir(root)
   end
 }
